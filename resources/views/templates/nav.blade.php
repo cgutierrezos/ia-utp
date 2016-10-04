3c0ac39742ad9fdc855c0df67fae411f9b0cd91c
cgutierrezos <cgutierrezos@utp.edu.co>
2016-10-04 18:44:15 -0500

    responsive movile

--- resources/views/templates/nav.blade.php
+++ resources/views/templates/nav.blade.php
@@ -1,38 +1,30 @@
-<nav class="navbar navbar-default navbar-static-top">
-    <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <!-- Collect the nav links, forms, and other content for toggling -->
-      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        <ul class="nav nav-tabs">
-          <li>
-              <a>
-                <img alt="A.I" width="100" height="30"    src="{{asset('imagenes/ai.png')}}">
-              </a>
-          </li>
-          <li role="presentation" class="active">
-            <a href="/">
-              <span class="glyphicon glyphicon-home" aria-hidden="true"></span> 
-              Inicio
-            </a>
-          </li>
-          <li role="presentation">
-            <a href="/algoritmos">
-              <span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span> 
-              Algoritmos
-            </a>
-          </li>
-          <li class="dropdown">
-            <a  class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-              <span class="glyphicon glyphicon-play-circle" aria-hidden="true"></span> Animacion <span class="caret"></span>
-            </a>
-            <ul class="dropdown-menu">
-              <li><a href="/animacion/anchura"> 
-                <span class="glyphicon glyphicon-minus" aria-hidden="true"></span>  Busqueda en anchura</a></li>
-              <li><a href="/animacion/profundidad">
-              <span class="glyphicon glyphicon-minus" aria-hidden="true"></span>  Busqueda en profundidad</a></li>
-            </ul>
-          </li>
-        </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
+<ul class="nav nav-tabs">
+  <li>
+      <a>
+        <img alt="A.I" width="100" height="30"    src="{{asset('imagenes/ai.png')}}">
+      </a>
+  </li>
+  <li role="presentation" class="active">
+    <a href="/">
+      <span class="glyphicon glyphicon-home" aria-hidden="true"></span> 
+      Inicio
+    </a>
+  </li>
+  <li role="presentation">
+    <a href="/algoritmos">
+      <span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span> 
+      Algoritmos
+    </a>
+  </li>
+  <li class="dropdown">
+    <a  class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+      <span class="glyphicon glyphicon-play-circle" aria-hidden="true"></span> Animacion <span class="caret"></span>
+    </a>
+    <ul class="dropdown-menu">
+      <li><a href="/animacion/anchura"> 
+        <span class="glyphicon glyphicon-minus" aria-hidden="true"></span>  Busqueda en anchura</a></li>
+      <li><a href="/animacion/profundidad">
+      <span class="glyphicon glyphicon-minus" aria-hidden="true"></span>  Busqueda en profundidad</a></li>
+    </ul>
+  </li>
+</ul>
