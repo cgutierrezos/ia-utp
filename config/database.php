4639f95eafd7ea5e9f4adb7ee4a349da680163f8
cgutierrezos <cgutierrezos@utp.edu.co>
2016-10-27 23:37:39 -0500

    pgsql

--- config/database.php
+++ config/database.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,9 +56,9 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_cf182bd76a9e94a'),
+            'username' => env('DB_USERNAME', 'be7da97c1ac24d'),
+            'password' => env('DB_PASSWORD', 'eceb51e2'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
