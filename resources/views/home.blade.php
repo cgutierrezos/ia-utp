4fd6a74552a89f001033aa0daf5745ebd0caedc1
cgutierrezos <cgutierrezos@utp.edu.co>
2016-11-03 17:16:40 -0500

    validaciones

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,19 +3,14 @@
 @section('title', "Bienvenido")
 
 @section('body')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Bienvenido</div>
 
-                <div class="panel-body">
-                    <p style="color:green;">
-                        Registro exitoso, Intenta loguearte con tu correo y password!
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+    @include('templates.banner_welcome', [
+        'id' => 'inicio',
+        'title' => 'Bienvenido',
+        'paragraph' => 'Registro exitoso, Intenta loguearte con tu correo y password!',
+        'aref' => '',
+        'astyle' => 'display:none',
+        'atitle' => 'siguiente'
+    ])
+
 @endsection
